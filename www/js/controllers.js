2fc1d879f1aad77779c46cc711d3a5d43400993d
Bruno Leone Alexandre Pacheco Corrêa <leone.analistadesistemas@outlook.com.br>
2016-10-19 02:02:19 -0200

    Add files via upload

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,12 +3,16 @@ var app = angular.module('app.controllers', ['ionic', 'ngCordova']) //, 'ngMessa
 /*==========================================
 =            Controller for App            =
 ==========================================*/
-app.controller('AppCtrl', function($scope, $timeout, $stateParams) {})
+app.controller('AppCtrl', function($scope, $timeout, $stateParams) {
+    $scope.title = 'Pagina';
+})
 
 /*===========================================
 =            Controller for Home            =
 ===========================================*/
-app.controller('HomeCtrl', function($scope, $stateParams) {})
+app.controller('HomeCtrl', function($scope, $stateParams) {
+
+})
 
 /*=============================================================
 =            Controller for Device Especifications            =
@@ -41,7 +45,7 @@ app.controller('deviceCtrl', function($scope, $cordovaDevice) {
     /*=============================================
     =            Controller for signup            =
     =============================================*/
-    app.controller('Signup', function($scope, $stateParams) {})
+app.controller('Signup', function($scope, $stateParams) {})
 
 /*=============================================
 =            Controller for search            =
@@ -50,9 +54,9 @@ app.controller('SearchCtrl', function($scope, $stateParams) {})
     /*====================================================================
     =            Controller for Scan of the qr code vs alunos            =
     ====================================================================*/
-    app.controller('ScanCtrl', function($scope, $cordovaBarcodeScanner, $cordovaDialogs, $cordovaVibration) {
-        $scope.scanBarcode = function() {
-            $cordovaBarcodeScanner.scan().then(function(imageData) {
+app.controller('ScanCtrl', function($scope, $cordovaBarcodeScanner, $cordovaDialogs, $cordovaVibration) {
+    $scope.scanBarcode = function() {
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
             //alert(imageData.text);
             console.log("Barcode Format -> " + imageData.format);
             console.log("Cancelled -> " + imageData.cancelled);
@@ -61,7 +65,7 @@ app.controller('SearchCtrl', function($scope, $stateParams) {})
         }, function(error) {
             alert(console.log("An error happened -> " + error));
         });
-        };
+    };
     /*document.addEventListener("deviceready", function() {
         $scope.scanBarcode = function() {
             $cordovaBarcodeScanner.scan().then(function(imageData) {
@@ -82,31 +86,31 @@ app.controller('SearchCtrl', function($scope, $stateParams) {})
         		// An error occurred
         	});
         }, false);*/
-    })
+})
 
 /*===============================================
 =            Controller for scanning            =
 ===============================================*/
 app.controller('dashStudentCtrl', function($scope, $stateParams) {
-    cordova.plugins.barcodeScanner.scan(
-        function(result) {
-            alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-        },
-        function(error) {
-            alert("Scanning failed: " + error);
-        }, {
+        cordova.plugins.barcodeScanner.scan(
+            function(result) {
+                alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+            },
+            function(error) {
+                alert("Scanning failed: " + error);
+            }, {
                 "preferFrontCamera": true, // iOS and Android
                 "showFlipCameraButton": true, // iOS and Android
                 "prompt": "Place a barcode inside the scan area", // supported on Android only
                 "formats": "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
                 "orientation": "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
             }
-            );
-})
-/*=====  End of Controller for scanning  ======*/
+        );
+    })
+    /*=====  End of Controller for scanning  ======*/
 
 
 /*============================================
@@ -151,30 +155,30 @@ app.controller('LoginCtrl', function($scope, $stateParams) { //, $timeout, $ioni
     /*========================================
     =            DASH FOR STUDENT            =
     ========================================*/
-    app.controller('dashStudentCtrl', function($scope, $stateParams) {})
+app.controller('dashStudentCtrl', function($scope, $stateParams) {})
     /*=====  End of DASH FOR STUDENT  ======*/
 
-    app.controller('generateQRCtrl', function($scope, $stateParams) {
+app.controller('generateQRCtrl', function($scope, $stateParams) {
         /*Gerar o código com
-        * type number
-        *- supported are all levels **1-40**
-        *- use **0** for the lowest complexity*
-        *
-        *## correction 
-        *- Integer **1** - Level L (Low)
-        *- Integer **0** - Level M (Medium)*            
-        *- Integer **3** - Level Q (Quartile)
-        *- Integer **2** - Level H (High)
-
-        *## input mode 
-        *- `NUMBER`: *0, 1, 2, 3, 4, 5, 6, 7, 8, 9*
-        *- `ALPHA_NUM`: *0–9, A–Z (upper-case only), space, $, %, *, +, -, ., /, :*
-        *- `8bit` (default): *[ISO 8859-1](http://en.wikipedia.org/wiki/ISO_8859-1)*
-        */
+         * type number
+         *- supported are all levels **1-40**
+         *- use **0** for the lowest complexity*
+         *
+         *## correction 
+         *- Integer **1** - Level L (Low)
+         *- Integer **0** - Level M (Medium)*            
+         *- Integer **3** - Level Q (Quartile)
+         *- Integer **2** - Level H (High)
+         
+         *## input mode 
+         *- `NUMBER`: *0, 1, 2, 3, 4, 5, 6, 7, 8, 9*
+         *- `ALPHA_NUM`: *0–9, A–Z (upper-case only), space, $, %, *, +, -, ., /, :*
+         *- `8bit` (default): *[ISO 8859-1](http://en.wikipedia.org/wiki/ISO_8859-1)*
+         */
 
         /*var typeNumber = $scope.typeNumber;
-        *var correction = $scope.correction;
-        *///var inputMode = $scope.inputMode;
+         *var correction = $scope.correction;
+         */ //var inputMode = $scope.inputMode;
 
         var qr = new QRCode(typeNumber, correction, inputMode);
         qr.addData(text);
@@ -183,23 +187,23 @@ app.controller('LoginCtrl', function($scope, $stateParams) { //, $timeout, $ioni
     /*=====================================
     =            TESTE NG-MENU            =
     =====================================*/
-    app.controller('ngMenuCtrl', function($scope, $timeout, $mdSidenav, $log) {
-        $scope.toggleLeft = buildDelayedToggler('left');
-        $scope.toggleRight = buildToggler('right');
-        $scope.isOpenRight = function() {
-            return $mdSidenav('right').isOpen();
-        };
+app.controller('ngMenuCtrl', function($scope, $timeout, $mdSidenav, $log) {
+    $scope.toggleLeft = buildDelayedToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function() {
+        return $mdSidenav('right').isOpen();
+    };
 
     /**
      * Supplies a function that will continue to operate until the
      * time is up.
      */
-     function debounce(func, wait, context) {
+    function debounce(func, wait, context) {
         var timer;
 
         return function debounced() {
             var context = $scope,
-            args = Array.prototype.slice.call(arguments);
+                args = Array.prototype.slice.call(arguments);
             $timeout.cancel(timer);
             timer = $timeout(function() {
                 timer = undefined;
@@ -212,14 +216,14 @@ app.controller('LoginCtrl', function($scope, $stateParams) { //, $timeout, $ioni
      * Build handler to open/close a SideNav; when animation finishes
      * report completion in console
      */
-     function buildDelayedToggler(navID) {
+    function buildDelayedToggler(navID) {
         return debounce(function() {
             // Component lookup should always be available since we are not using `ng-if`
             $mdSidenav(navID)
-            .toggle()
-            .then(function() {
-                $log.debug("toggle " + navID + " is done");
-            });
+                .toggle()
+                .then(function() {
+                    $log.debug("toggle " + navID + " is done");
+                });
         }, 200);
     }
 
@@ -227,30 +231,30 @@ app.controller('LoginCtrl', function($scope, $stateParams) { //, $timeout, $ioni
         return function() {
             // Component lookup should always be available since we are not using `ng-if`
             $mdSidenav(navID)
-            .toggle()
-            .then(function() {
-                $log.debug("toggle " + navID + " is done");
-            });
+                .toggle()
+                .then(function() {
+                    $log.debug("toggle " + navID + " is done");
+                });
         }
     }
 })
-    app.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
-        $scope.close = function() {
+app.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function() {
         // Component lookup should always be available since we are not using `ng-if`
         $mdSidenav('left').close()
-        .then(function() {
-            $log.debug("close LEFT is done");
-        });
+            .then(function() {
+                $log.debug("close LEFT is done");
+            });
 
     };
 })
-    app.controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
-        $scope.close = function() {
+app.controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function() {
         // Component lookup should always be available since we are not using `ng-if`
         $mdSidenav('right').close()
-        .then(function() {
-            $log.debug("close RIGHT is done");
-        });
+            .then(function() {
+                $log.debug("close RIGHT is done");
+            });
     };
 });
 /**
@@ -258,7 +262,7 @@ app.controller('LoginCtrl', function($scope, $stateParams) { //, $timeout, $ioni
 	Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
 	**/
 
-    /*=====  End of TESTE NG-MENU  ======*/
+/*=====  End of TESTE NG-MENU  ======*/
 
 /*==========================================
 =            Controller for map            =
@@ -288,5 +292,73 @@ app.controller('MapCtrl', function($scope, $ionicLoading, $cordovaGeolocation) {
         });
     };
 });
-
 /*=====  End of Controller for map  ======*/
+
+/*----------  Controller for connectionCtrl  ----------*/
+app.controller('connectionCtrl', function($scope, $log, $stateParams, $cordovaNetwork, $rootScope) {
+
+    //======ngCordova events========//
+    document.addEventListener("deviceready", function() {
+
+        var type = $cordovaNetwork.getNetwork()
+
+        var isOnline = $cordovaNetwork.isOnline()
+
+        var isOffline = $cordovaNetwork.isOffline()
+
+
+        // listen for Online event
+        $rootScope.$on('$cordovaNetwork:online', function(event, networkState) {
+            var onlineState = networkState;
+        })
+
+        // listen for Offline event
+        $rootScope.$on('$cordovaNetwork:offline', function(event, networkState) {
+            var offlineState = networkState;
+        })
+
+    }, false);
+    //=======end ngCordova events===========//
+
+    /*$scope.checkConnection = function($scope) {
+            var netWorkState = navigator.connection.type;
+
+            var states = {};
+
+            states[Connection.UNKNOWN] = 'Online';
+            states[Connection.ETHERNET] = 'Ethernet';
+            states[Connection.WIFI] = 'WiFi';
+            states[Connection.CELL_2G] = '2G';
+            states[Connection.CELL_3G] = '3G';
+            states[Connection.CELL_4G] = '4G';
+            states[Connection.CELL] = 'Genérica';
+            states[Connection.NONE] = 'Offline';
+
+            //alert('Conexão: ' + states[netWorkState], '', '', 'OK');
+        }
+        //checkConnection();
+
+    //caso offline
+    //--document.addEventListener("offline", onOffline, false);
+
+    function onOffline() {
+        // Handle the offline event 
+        console.log("lost connection");
+    }
+
+    //caso online
+    //--document.addEventListener("online", onOnline, false);
+
+    function onOnline() {
+        // Handle the online event 
+        var networkState = navigator.connection.type;
+
+        if (networkState !== Connection.NONE) {
+            if (dataFileEntry) {
+                tryToUploadFile();
+            }
+        }
+        display('Connection type: ' + networkState);
+    }*/
+});
+/*---------- End for Controller for  ----------*/
